656911a9d256d44aabfbd5edc4162b63dd941df2
Simon Zettergren <zettergren.simon@gmail.com>
2024-07-09 15:34:30 +0200

    better cli, removed some intensity generation methods

--- main.py
+++ main.py
@@ -1,51 +1,76 @@
-from enum import Enum
+import argparse
+from enum import auto, Enum
 import sys
 
 from PIL import Image
 
 
-class GenerationMethod(Enum):
-    Brightness = 0
-    Sum = 1
-    Multiply = 2
-
 class Config:
     path: str
-    w: int
-    h: int
-    save: bool = True
-    print: bool = False
-    method: GenerationMethod = GenerationMethod.Brightness
+    width: int
+    height: int
+    save: bool
+    print: bool
 
+    def __init__(self):
+        def positive_int(value):
+            value = int(value)
+            if value < 0:
+                raise argparse.ArgumentTypeError(f"{value} is not positive")
+            return value
+
+        parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter,
+                                         description="Generate ascii from an image")
+
+        parser.add_argument("filepath", type=str, help="the filepath to the picture to be converted")
+        parser.add_argument("height", type=positive_int, nargs="?", default=20,
+                            help="the height of the ascii image in characters (default: 20)")
+        parser.add_argument("width", type=positive_int, nargs="?", default=0,
+                            help="the width of the ascii image in characters (default: whatever keeps the original ratio)")
+
+        parser.add_argument("--print", "-p", action="store_true", help="print the generated ascii image to stdout")
+        parser.add_argument("--write", "-w", action="store_true", help="write the generated ascii image to 'ascii.txt'")
+
+        args = parser.parse_args()
+
+        self.path = args.filepath
+        self.height = args.height
+        self.width = args.width
+        self.save = args.write
+        self.print = True if not args.write else args.print
+
+
+def luma(pixel) -> float:
+    r, g, b, _ = pixel;
+    return (r*0.299 + g*0.587 + b*0.114) / 255.0
 
 def run(cfg: Config):
-    pixels = list()
+    pixels = []
     with Image.open(cfg.path) as img:
-        w = cfg.w
-        h = cfg.h
+        h = cfg.height
+        w = cfg.width if cfg.width else int(img.size[0] * (h/img.size[1]) * 2)
         img = img.resize((w, h))
         pixels = list(img.getdata())
 
-    generate = generation_method(cfg.method)
     ascii = ""
     for y in range(h):
         for x in range(w):
-            value = generate(pixels[y*w + x])
-            if value > 0.99:
+            l = luma(pixels[y*w + x])
+            if l > 0.99:
                 ascii += "#"
-            elif value > 0.88:
+            elif l > 0.88:
                 ascii += "%"
-            elif value > 0.77:
+            elif l > 0.77:
                 ascii += "X"
-            elif value > 0.66:
+            elif l > 0.66:
                 ascii += "="
-            elif value > 0.55:
+            elif l > 0.55:
                 ascii += "+"
-            elif value > 0.44:
+            elif l > 0.44:
                 ascii += "-"
-            elif value > 0.33:
+            elif l > 0.33:
                 ascii += ","
-            elif value > 0.22:
+            elif l > 0.22:
                 ascii += "."
             else:
                 ascii += " "
@@ -59,125 +84,7 @@ def run(cfg: Config):
             f.write(ascii)
         print("ascii saved to 'ascii.txt'")
 
-def generation_method(method: GenerationMethod):
-    def colors(pixel):
-        r = pixel[0] / 255.0
-        g = pixel[1] / 255.0
-        b = pixel[2] / 255.0
-
-        return r, g, b
-
-    def brightness(pixel):
-        r, g, b = colors(pixel)
-        return r*0.299 + g*0.587 + b*0.114
-
-    def sum(pixel):
-        r, g, b = colors(pixel)
-        return (r + b + g) / 3.0
-
-    def multiply(pixel):
-        r, g, b = colors(pixel)
-        return r * g * b
-
-    match method:
-        case GenerationMethod.Brightness:
-            return brightness
-        case GenerationMethod.Sum:
-            return sum
-        case GenerationMethod.Multiply:
-            return multiply
-
 
 if __name__ == "__main__":
     cfg = Config()
-
-    args = sys.argv
-    args.pop(0)
-
-    if len(args) == 0:
-        print("No filepath specified")
-        print("Try '--help' for more information")
-        sys.exit(1)
-
-    opts = list()
-    i = 0
-    while i < len(args):
-        arg = args[i]
-        if arg[0] == "-":
-            if arg[1] != "-" and len(arg) > 2:
-                for c in arg[1:]:
-                    opts.append("-" + c)
-            else:
-                opts.append(args[i])
-            args.pop(i)
-        else:
-            i += 1
-
-    if "-h" in opts or "--help" in opts:
-        print("Generates ascii from a picture")
-        print("Usage: [OPTIONS] [FILEPATH]")
-        print("   or: [OPTIONS] [FILEPATH] [SCALE]")
-        print("   or: [OPTIONS] [FILEPATH] [WIDTH] [HEIGHT]")
-        print()
-        print("FILEPATH:   filepath to a picture")
-        print("SCALE:      a scale to change the amount of pixels by")
-        print("WIDTH:      an amount of pixels to set the width to")
-        print("HEIGHT:     an amount of pixels to set the height to")
-        print()
-        print("OPTIONS:")
-        print()
-        print("    -b      uses percieved brightnes for generating ascii  (default)")
-        print("    -m      uses pixel multiplication for genrating ascii")
-        print("    -s      uses pixel sum for generating ascii")
-        print()
-        print("    -p      prints the ascii")
-        print("    -w      saves the ascii to a text file")
-        print()
-        print("    -h --help   displays this help and exits")
-        print()
-        sys.exit(0)
-
-    if "-m" in opts:
-        cfg.method = GenerationMethod.Multiply
-    if "-s" in opts:
-        cfg.method = GenerationMethod.Sum
-    if "-b" in opts:
-        cfg.method = GenerationMethod.Brightness
-
-    if "-p" in opts:
-        cfg.save = False
-        cfg.print = True
-    if "-w" in opts:
-        cfg.save = True
-
-    if len(args) > 0:
-        cfg.path = args[0]
-    if len(args) == 1:
-        args.append("0.1")
-    if len(args) == 2:
-        try:
-            scale = float(args[1])
-        except:
-            print(f"SCALE '{args[1]}' is not a float")
-            sys.exit(1)
-        else:
-            with Image.open(cfg.path) as img:
-                w, h = img.size;
-                cfg.w = int(w * scale)
-                cfg.h = int(h * scale / 2.3)
-    elif len(args) == 3:
-        try:
-            cfg.w = int(args[1])
-        except:
-            print(f"WIDTH '{args[1]}' is not an int")
-            sys.exit(1)
-        try:
-            cfg.h = int(args[2])
-        except:
-            print(f"HEIGHT '{args[2]}' is not an int")
-            sys.exit(1)
-    else:
-        print("Too many arguments where provided")
-        sys.exit(1)
-
     run(cfg)
